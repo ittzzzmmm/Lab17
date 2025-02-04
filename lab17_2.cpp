1454b731bfa5bc11b5a27caad7f7b0503f6d147f
ittzzzmmm <ittzzzmmm@gmail.com>
2025-02-04 17:02:47 +0700

    ittzm

--- lab17_2.cpp
+++ lab17_2.cpp
@@ -1,18 +1,18 @@
 #include<iostream>
 using namespace std;
 
-________ myString(_____________________){
-	_______ = new ____________;    
-	for(int i = 0; i < N;i++) ____________ = 'A'+i;
-	__________ = 0;
+void myString(char *&p,int N){
+	p = new char;  //ชี้ไป address in heap  
+	for(int i = 0; i < N;i++) *(p+i) = 'A'+i;  //ถ้าเป็น char มันจะอ่านจนกว่าจะเจอ 0,\0 
+	*(p+N) = 0; // ใส่เพื่อหยุดการอ่าน
 }
 
 int main(){
-	char *p;
-	int N;	
+	char *p; // สร้าง pointer p ชี้ไปที่ char (ยังไม่ชี้)
+	int N;	// กำหนดความยาวม
 	cout << "N = ";
 	cin >> N;		
-	myString(p,N);
+	myString(p,N); // call function
 	cout << p;	
 	delete [] p;
 	return 0;
